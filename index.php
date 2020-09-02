7237314137e286d352f6b1012236176200a0a4f1
Toseef Tariq <dapspakistan@gmail.com>
2020-09-02 06:29:52 +0500

    create index
    test

---
+++ index.php
@@ -0,0 +1,135 @@
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Toseef Tariq Portfolio</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="https://benjaminthedev.github.io/assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="https://benjaminthedev.github.io/assets/css/noscript.css" /></noscript>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+
+</head>
+
+<body class="is-preload">
+	<!-- Header -->
+	<section id="header">
+		<div class="inner">
+			<h1>Hi, I'm <strong>Toseef Tariq</strong>, <br />
+				Remote Lead Developer from the KSA.</h1>
+
+			<p> Custom Website Development / Ecommerce Development / WordPress Development / Hybrid Application Development @ <em><a
+						href="https://www.daps.pk" target="_blank">DAPS</a></em></p>
+			<ul class="actions special">
+				<li><a href="#one" class="button scrolly">Core Skills</a></li>
+				<li><a href="#two" class="button scrolly">Projects</a></li>
+				<li><a href="#footer" class="button scrolly">Contact Me</a></li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- One -->
+	<section id="one" class="main style2">
+		<div class="container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<ul class="major-icons">
+
+						<li><span class="icon"><i class="fab fa-html5"></i></span></li>
+						<li><span class="icon"><i class="fab fa-css3-alt"></i></span></li>
+						<li><span class="icon"><i class="fab fa-js"></i></span></li>
+						<li><span class="icon"><i class="fab fa-react"></i></span></li>
+						<li><span class="icon"><i class="fab fa-node"></i></span></li>
+						<li><span class="icon"><i class="fab fa-sass"></i></span></li>
+						<li><span class="icon"><i class="fab fa-less"></i></span></li>
+						<li><span class="icon"><i class="fab fa-gulp"></i></span></li>
+						<li><span class="icon"><i class="fab fa-grunt"></i></span></li>
+						<li><span class="icon"><i class="fab fa-wordpress-simple"></i></span></li>
+						<li><span class="icon"><i class="fab fa-php"></i></span></li>
+						<li><span class="icon"><i class="fab fa-bootstrap"></i></span></li>
+						<li><span class="icon"><i class="fab fa-npm"></i></span></li>
+						<li><span class="icon"><i class="fab fa-trello"></i></span></li>
+						<li><span class="icon"><i class="fab fa-yarn"></i></span></li>
+						<li><span class="icon"><i class="fab fa-adobe"></i></span></li>
+						<li><span class="icon"><i class="fab fa-github"></i></span></li>
+						<li><span class="icon"><i class="fab fa-codepen"></i></span></li>
+					</ul>
+				</div>
+				<div class="col-6 col-12-medium">
+					<header class="major core-skills">
+						<h2>Core Skills<br />
+						</h2>
+					</header>
+					<p>Hi there, I’m  <strong>Toseef Tariq</strong> an independent
+						lead  developer based in beautiful
+						KSA. </p>
+
+					<p>With over 5 years of hands-on web development, I specialise in creating and building
+						carefully-crafted,
+						responsive websites and applications.</p>
+
+					<p>I’m super passionate about the web and have a keen eye when it comes to design. My previous work
+						has been experienced by
+						thousands of users worldwide every year, using the best front-end technologies such as <strong> HTML5,
+						CSS3, WordPress, PHP
+						Javascript, ReactJS and NodeJS</strong></p>
+
+					<p>I strive for design and code perfection. My home office is packed with the latest kit ensuring
+						that your project is
+						current and works flawlessly on all devices.</p>
+
+						<a href="#footer" class="button scrolly">Contact Me</a>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+		<?php include 'http://daps.pk/projects.php';?>
+
+				 
+
+			
+
+
+
+
+	<!-- Footer -->
+	<section id="footer">
+		<ul class="icons">
+			<li><a href="https://www.linkedin.com/in/toseef-tariq-576478b1/" target="_blank"
+					class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+			
+			<li><a href="https://github.com/toseef-tariq" target="_blank" class="icon alt fa-github"><span
+						class="label">GitHub</span></a></li>
+			<li><a href="https://www.facebook.com/daps.pk.75/" target="_blank"
+					class="icon alt fa-facebook"><span class="label">FaceBook</span></a></li>
+			<li><a href="http://wa.link/22275a" target="_blank" class="icon alt fa-whatsapp"><span
+						class="label">Whatsapp</span></a></li>
+			<li><a href="mailto:toseeftariq300@gmail.com?subject=Hello Ben" class="icon alt fa-envelope"><span
+						class="label">Email</span></a></li>
+			<br />
+			<li class="course-link"><a
+					href="http://wa.link/22275a"
+					target="_blank"><span class="label">Click Here to Chat on Whatsapp</span></a></li>
+		</ul>
+		<ul class="copyright">
+			<li>&copy; ToseefTariq </li>
+		</ul>
+	</section>
+
+	<!-- Scripts -->
+	<script src="https://benjaminthedev.github.io/assets/js/jquery.min.js"></script>
+	<script src="https://benjaminthedev.github.io/assets/js/jquery.scrolly.min.js"></script>
+	<script src="https://benjaminthedev.github.io/assets/js/browser.min.js"></script>
+	<script src="https://benjaminthedev.github.io/assets/js/breakpoints.min.js"></script>
+	<script src="https://benjaminthedev.github.io/assets/js/util.js"></script>
+	<script src="https://benjaminthedev.github.io/assets/js/main.js"></script>
+
+</body>
+
+</html>
